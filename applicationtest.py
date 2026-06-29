56caa633faf36a9698575ffd1357c5599781c3ea
root <root@ip-172-31-9-82.ap-southeast-2.compute.internal>
2026-06-29 11:06:18 +0000

    seccond commit

---
+++ applicationtest.py
@@ -0,0 +1,23 @@
+import unittest
+from applicationtest import application
+
+class FlaskApplicationTest(unittest.TestCase):
+
+    def setUp(self):
+        application.testing = True
+        self.client = application.test_client()
+
+    def test_home_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_content(self):
+        response = self.client.get("/")
+        self.assertIn(b"TechNova Solutions", response.data)
+
+    def test_content_type(self):
+        response = self.client.get("/")
+        self.assertEqual(response.content_type, "text/html; charset=utf-8")
+
+if __name__ == "__main__":
+    unittest.main()
